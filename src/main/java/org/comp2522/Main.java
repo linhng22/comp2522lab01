0679cde810e9016faf1ec36625aee32bb9fb0f4d
Linh Nguyen <knguyen142@my.bcit.ca>
2022-09-07 21:59:20 -0600

    fix checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * Display a greeting.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, I am Linh and I love guinea pigs.");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, I am Linh and I love guinea pigs.");
+  }
 }
